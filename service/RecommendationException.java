40d7d144568fd77e92272fd31909763f3ddd2a6b
rayhm6419 <105839955+rayhm6419@users.noreply.github.com>
2022-06-02 16:43:26 -0700

    Add files via upload

---
+++ service/RecommendationException.java
@@ -0,0 +1,8 @@
+package com.aoshine.demo.service;
+
+
+    public class RecommendationException extends RuntimeException {
+        public RecommendationException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
